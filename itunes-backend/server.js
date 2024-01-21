cd3c8bab5fca48dff6e84e0a5944ce355c5058a3
Tumisho Makgeru <makgerutumisho55@gmail.com>
2024-01-21 19:46:44 +0200

    Search API completed

--- itunes-backend/server.js
+++ itunes-backend/server.js
@@ -30,6 +30,160 @@ app.get("/search", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+app.get("/search/artist", async (req, res) => {
+  const searchTerm = req.query.term;
+
+  try {
+    const response = await axios.get("https://itunes.apple.com/search", {
+      params: {
+        term: searchTerm,
+        entity: "musicArtist",
+      },
+    });
+
+    const results = response.data.results;
+    res.json(results);
+  } catch (error) {
+    console.error(
+      "Error fetching data for music artists from iTunes API:",
+      error.message
+    );
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/search/songs", async (req, res) => {
+  const searchTerm = req.query.term;
+
+  try {
+    const response = await axios.get("https://itunes.apple.com/search", {
+      params: {
+        term: searchTerm,
+        media: "music",
+      },
+    });
+
+    const results = response.data.results;
+    res.json(results);
+  } catch (error) {
+    console.error(
+      "Error fetching data for music artists from iTunes API:",
+      error.message
+    );
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/search/album", async (req, res) => {
+  const searchTerm = req.query.term;
+
+  try {
+    const response = await axios.get("https://itunes.apple.com/search", {
+      params: {
+        term: searchTerm,
+        entity: "album",
+      },
+    });
+
+    const results = response.data.results;
+    res.json(results);
+  } catch (error) {
+    console.error(
+      "Error fetching data for music artists from iTunes API:",
+      error.message
+    );
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/search/mix", async (req, res) => {
+  const searchTerm = req.query.term;
+
+  try {
+    const response = await axios.get("https://itunes.apple.com/search", {
+      params: {
+        term: searchTerm,
+        entity: "mix",
+      },
+    });
+
+    const results = response.data.results;
+    res.json(results);
+  } catch (error) {
+    console.error(
+      "Error fetching data for music artists from iTunes API:",
+      error.message
+    );
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/search/podcasts", async (req, res) => {
+  const searchTerm = req.query.term;
+
+  try {
+    const response = await axios.get("https://itunes.apple.com/search", {
+      params: {
+        term: searchTerm,
+        entity: "podcast",
+      },
+    });
+
+    const results = response.data.results;
+    res.json(results);
+  } catch (error) {
+    console.error(
+      "Error fetching data for music artists from iTunes API:",
+      error.message
+    );
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/search/musicVideos", async (req, res) => {
+  const searchTerm = req.query.term;
+
+  try {
+    const response = await axios.get("https://itunes.apple.com/search", {
+      params: {
+        term: searchTerm,
+        entity: "musicVideo",
+      },
+    });
+
+    const results = response.data.results;
+    res.json(results);
+  } catch (error) {
+    console.error(
+      "Error fetching data for music artists from iTunes API:",
+      error.message
+    );
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/search/movie", async (req, res) => {
+  const searchTerm = req.query.term;
+
+  try {
+    const response = await axios.get("https://itunes.apple.com/search", {
+      params: {
+        term: searchTerm,
+        entity: "movie",
+      },
+    });
+
+    const results = response.data.results;
+    res.json(results);
+  } catch (error) {
+    console.error(
+      "Error fetching data for music artists from iTunes API:",
+      error.message
+    );
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 app.listen(PORT, () => {
   console.log(`Server is listening on port http://localhost:${PORT}`);
 });
--- itunes-frontend/src/components/SearchLayout/SearchLayout.jsx
+++ itunes-frontend/src/components/SearchLayout/SearchLayout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { artistCards } from "../Cards/artistCards";
 
 export const SearchLayout = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -21,6 +22,110 @@ export const SearchLayout = () => {
     }
   };
 
+  const handleArtistSearch = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:8080/search/artist?term=${searchTerm}`
+      );
+      const results = await response.json();
+      setSearchResults(results);
+      console.log(results);
+    } catch (error) {
+      console.error("Error fetching data from backend:", error.message);
+      // Handle error appropriately, e.g., display an error message to the user
+    }
+  };
+
+  const handleSongSearch = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:8080/search/songs?term=${searchTerm}`
+      );
+      const results = await response.json();
+      setSearchResults(results);
+      console.log(results);
+    } catch (error) {
+      console.error("Error fetching data from backend:", error.message);
+      // Handle error appropriately, e.g., display an error message to the user
+    }
+  };
+
+  const handleAlbumSearch = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:8080/search/album?term=${searchTerm}`
+      );
+      const results = await response.json();
+      setSearchResults(results);
+      console.log(results);
+    } catch (error) {
+      console.error("Error fetching data from backend:", error.message);
+      // Handle error appropriately, e.g., display an error message to the user
+    }
+  };
+
+  const handlePlaylistSearch = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:8080/search/mix?term=${searchTerm}`
+      );
+      const results = await response.json();
+      setSearchResults(results);
+      console.log(results);
+    } catch (error) {
+      console.error("Error fetching data from backend:", error.message);
+      // Handle error appropriately, e.g., display an error message to the user
+    }
+  };
+
+  const handlePodcastSearch = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:8080/search/podcasts?term=${searchTerm}`
+      );
+      const results = await response.json();
+      setSearchResults(results);
+      console.log(results);
+    } catch (error) {
+      console.error("Error fetching data from backend:", error.message);
+      // Handle error appropriately, e.g., display an error message to the user
+    }
+  };
+
+  const handleMusicVideoSearch = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:8080/search/musicVideos?term=${searchTerm}`
+      );
+      const results = await response.json();
+      setSearchResults(results);
+      console.log(results);
+    } catch (error) {
+      console.error("Error fetching data from backend:", error.message);
+      // Handle error appropriately, e.g., display an error message to the user
+    }
+  };
+
+  const handleMovieSearch = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:8080/search/movie?term=${searchTerm}`
+      );
+      const results = await response.json();
+      setSearchResults(results);
+      console.log(results);
+    } catch (error) {
+      console.error("Error fetching data from backend:", error.message);
+      // Handle error appropriately, e.g., display an error message to the user
+    }
+  };
   return (
     <div>
       <form onSubmit={handleSearch}>
@@ -35,6 +140,16 @@ export const SearchLayout = () => {
         />
         <button type="submit">Search</button>
       </form>
+
+      <button onClick={handleSearch}>All</button>
+      <button onClick={handleArtistSearch}>Artists</button>
+      <button onClick={handleSongSearch}>Songs</button>
+      <button onClick={handleAlbumSearch}>Albums</button>
+      <button onClick={handlePlaylistSearch}>Playlists</button>
+      <button onClick={handlePodcastSearch}>Podcast and shows</button>
+      <button onClick={handleMusicVideoSearch}>Music Videos</button>
+      <button onClick={handleMovieSearch}>Films</button>
+
       <div className="jumbotron-container">Search page</div>
       <div className="d-flex justify-content-center">
         <div className="jumbotron-small">Smaller content</div>
